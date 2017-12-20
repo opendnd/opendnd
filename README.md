90e0d45dbb0c785ebc7ac820c5814a9f9c596349
Drew Morris <dhmorris@gmail.com>
2017-12-20 18:47:29 -0500

    Adding a single cli for all generators

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # opendnd
 This is the main repository and CLI which combines the others into one.
+
+```
+npm i -g opendnd
+dnd
+```
---
+++ test/lib/opendnd.test.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+
+const expect = require('chai').expect;
+const path = require('path');
+const rootDir = path.join(__dirname, '..', '..');
+const libDir = path.join(rootDir, 'lib');
+const opendnd = require(path.join(libDir, 'opendnd'));
+
+describe('opendnd', () => {
+  it('is an object', () => {
+    expect(opendnd).to.be.an('object');
+  });
+});
